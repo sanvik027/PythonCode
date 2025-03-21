82c5cecbf322dd091ac779750a6b098e346e1e1c
sanvik027 <sanvik018@gmail.com>
2025-03-21 12:14:05 +0530

    Renamed File

--- Lists/duplicate_lst.py
+++ Lists/duplicate_lst.py
@@ -3,5 +3,6 @@ def remove_duplicates(lst):
     unique_lst = [x for x in lst if not((x in seen)or seen.add(x))]
     return unique_lst
 
+
 num = [1, 2, 3, 2, 4, 5, 1, 6]
 print(remove_duplicates(num))
